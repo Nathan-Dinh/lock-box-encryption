76c6b440d9cfd32e022cb00b0ea75c42a50acffb
Nathan Dinh <nathan.dinh03@outlook.com>
2024-03-27 00:32:00 -0400

    Added database service

---
+++ my-app/src/service/database.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DatabaseService {
+  db: any;
+
+  createDatabase(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const request = indexedDB.open("LBEDB", 1);
+      request.onerror = (event) => {
+        console.error("Error in creating database.");
+        reject("Error in creating database");
+      }
+      request.onsuccess = (event) => {
+        // @ts-ignore
+        this.db = event.target.result;
+        resolve(this.db);
+      }
+      request.onupgradeneeded = (event) => {
+        // @ts-ignore
+        this.db = event.target.result;
+        this.db.createObjectStore("user", {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+        this.db.createObjectStore("gallery_item", {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+      }
+    });
+  }
+
+  initDatabase() {
+    this.createDatabase().then((data) => {
+      console.log("Database created successfully " + data);
+    }).catch((e) => {
+      console.log("Error in database creation: " + e.message);
+    });
+  }
+}
