4bddd61c135295ce16c4157a3ee7272c3383fbca
Nathan Dinh <nathan.dinh03@outlook.com>
2024-03-28 02:56:56 -0400

    Renamed database service path

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DatabaseService } from '../service/database.service'; 
+import { DatabaseService } from '../services/database.service' 
 
 @Component({
   selector: 'app-root',
