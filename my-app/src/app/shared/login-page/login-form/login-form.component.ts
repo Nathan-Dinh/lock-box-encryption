e3a2838a4d69b9908ba49f063d615983122ca7e8
Nathan Dinh <nathan.dinh03@outlook.com>
2024-03-28 02:58:14 -0400

    Implemented form validation logic

--- my-app/src/app/shared/login-page/login-form/login-form.component.ts
+++ my-app/src/app/shared/login-page/login-form/login-form.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core'
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators
+} from '@angular/forms'
+import { CookieService } from 'ngx-cookie-service'
+import { UserDalService } from '../../../../services/user.dal.service'
+import { User } from '../../../../models/user.model'
+import { CookieEncryptionService } from '../../../../services/crypto/cookie-encryption.service'
 
 @Component({
   selector: 'login-form',
   standalone: true,
-  imports: [],
+  providers: [CookieService],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
+  userDal = inject(UserDalService)
+  CEService = inject(CookieEncryptionService)
+  cookieService : CookieService
+  user: User = new User('', '')
+  isFormValid = true
+
+  constructor(private builder: FormBuilder, cookieService : CookieService) {
+    this.cookieService = cookieService
+  }
+
+  userForm = this.builder.group({
+    userName: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    keepMeLoggedIn: ['', []],
+  })
+
+  addUserClickHandler(): void {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value)
+      if (this.userForm.value.keepMeLoggedIn) {
+        this.cookieService.set('test', this.CEService.encryptCookie("dsa") )
+      }
+    } else {
+      this.isFormValid = false
+    }
+  }
 }
