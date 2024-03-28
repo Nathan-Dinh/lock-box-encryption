09e154c80622ee7a1872635a43050aa335cd1970
Nathan Dinh <nathan.dinh03@outlook.com>
2024-03-28 02:54:56 -0400

    Added route guard to routes

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router'
 import { LoginComponent } from './pages/login/login.component'
 import { InventoryComponent } from './pages/inventory/inventory.component'
+import { authGuard } from '../services/auth/auth.guard'
 
 export const routes: Routes = [
   {
     path: '',
     component: InventoryComponent,
     title: 'Inventory Component',
+    canActivate: [authGuard]
   },
   {
     path: 'login',
