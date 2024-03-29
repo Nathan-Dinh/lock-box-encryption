d55517e4ef36bdb49ef7c49290d9d94b70fb0e4e
John Doe <nathan.dinh03@outlook.com>
2024-03-28 23:25:14 -0400

    Added valdiation logic

--- my-app/src/app/shared/create-user-page/create-user-form/create-user-form.component.ts
+++ my-app/src/app/shared/create-user-page/create-user-form/create-user-form.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core'
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+} from '@angular/forms'
+import { JsonPipe } from '@angular/common'
 
 @Component({
   selector: 'create-user-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './create-user-form.component.html',
-  styleUrl: './create-user-form.component.css'
+  styleUrl: './create-user-form.component.css',
 })
 export class CreateUserFormComponent {
+  builder = inject(FormBuilder)
 
+  createUserForm = this.builder.group({
+    userName: ['', [Validators.required]],
+    passwordGroup: this.builder.group(
+      {
+        _password: ['', [Validators.required]],
+        _password2: ['', [Validators.required]],
+      },{ validators: this.matchPassword }
+    ),
+  })
+
+  // userName: ['', [Validators.required]],
+  // passwordGroup: this.builder.group(
+  //   {
+  //     _password: ['', [Validators.required]],
+  //     _password2: ['', [Validators.required]],
+  //   },{ validator: this.matchPassword }
+  // ),
+
+  matchPassword(control: AbstractControl) {
+    console.log('trigger')
+    return { passwordGroupError: true }
+  }
 }
