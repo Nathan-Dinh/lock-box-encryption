5e66dcedb535124ce9b4baaa788ae92dfaa54722
John Doe <nathan.dinh03@outlook.com>
2024-04-02 17:10:44 -0400

    Added bottom home header

---
+++ my-app/src/app/shared/components/bottom-home-header/bottom-home-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bottom-home-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './bottom-home-header.component.html',
+  styleUrl: './bottom-home-header.component.css'
+})
+export class BottomHomeHeaderComponent {
+
+}
