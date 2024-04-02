32987e2626e95a84d50a02b959accfd54f8b57c9
John Doe <nathan.dinh03@outlook.com>
2024-04-02 17:09:32 -0400

    Changed component for path " "

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router'
 import { LoginComponent } from './pages/login/login.component'
-import { InventoryComponent } from './pages/inventory/inventory.component'
 import { authGuard } from '../services/auth/auth.guard'
 import { CreateUserComponent } from './pages/create-new-user/create-user.component'
+import { HomeComponent } from './pages/home/home.component'
 
 export const routes: Routes = [
   {
     path: '',
-    component: InventoryComponent,
-    title: 'Inventory Component',
+    component: HomeComponent,
+    title: 'Gallery',
     canActivate: [authGuard]
   },
   {
