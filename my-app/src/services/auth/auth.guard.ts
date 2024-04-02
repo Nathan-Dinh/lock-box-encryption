83b4b69e72684c81fb3d06da69e4fe879604079e
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-02 00:32:10 -0400

    Updated authorization logic

--- my-app/src/services/auth/auth.guard.ts
+++ my-app/src/services/auth/auth.guard.ts
@@ -1,9 +1,14 @@
 import { inject } from '@angular/core'
 import { CanActivateFn } from '@angular/router'
 import { Router } from '@angular/router'
+import { AuthControlService } from './auth-control.service'
 
 export const authGuard: CanActivateFn = (route, state) => {
   const ROUTER = inject(Router)
+  const AUTH_CONTROL = inject(AuthControlService)
+  if(AUTH_CONTROL.isAuth){
+    return true
+  }
   ROUTER.navigate(['login'])
   return false
 }
