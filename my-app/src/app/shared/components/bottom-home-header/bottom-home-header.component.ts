ea756469216d2226b38cfc077cdf603876ff39d0
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-02 21:53:36 -0400

    Replaced href to router link

--- my-app/src/app/shared/components/bottom-home-header/bottom-home-header.component.ts
+++ my-app/src/app/shared/components/bottom-home-header/bottom-home-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'bottom-home-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './bottom-home-header.component.html',
   styleUrl: './bottom-home-header.component.css'
 })
