130cd80ed57839bdc5495698053fb05949580c97
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-02 21:52:54 -0400

    Added encryption child page

---
+++ my-app/src/app/pages/home/encryption/encryption.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'encryption-sub-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './encryption.component.html',
+  styleUrl: './encryption.component.css'
+})
+export class EncryptionComponent {
+
+}
