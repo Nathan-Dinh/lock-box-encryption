f83f761ab3f5a6327f4c15d6e850830677bb36b3
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-03 00:10:31 -0400

    Added router module

--- my-app/src/app/shared/components/home-header/home-header.component.ts
+++ my-app/src/app/shared/components/home-header/home-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'home-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home-header.component.html',
   styleUrl: './home-header.component.css',
 })
