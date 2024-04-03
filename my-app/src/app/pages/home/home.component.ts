defd3e4e45cb6178944b1331c02a9efa999e6e28
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-02 21:52:37 -0400

    Changed selector name and imported router outlet

--- my-app/src/app/pages/home/home.component.ts
+++ my-app/src/app/pages/home/home.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeHeaderComponent } from '../../shared/components/home-header/home-header.component';
 import { BottomHomeHeaderComponent } from '../../shared/components/bottom-home-header/bottom-home-header.component';
+import { RouterOutlet } from '@angular/router'
 
 @Component({
-  selector: 'app-home',
+  selector: 'home-page',
   standalone: true,
-  imports: [HomeHeaderComponent,BottomHomeHeaderComponent],
+  imports: [HomeHeaderComponent, BottomHomeHeaderComponent, RouterOutlet],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-
-export class HomeComponent {
-
-}
+export class HomeComponent {}
