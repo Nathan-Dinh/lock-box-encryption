e23a104c5c91cc94c1d5012d7cbdc7e89da14a21
JindoKimKor <jindokim.kor@gmail.com>
2024-04-04 18:09:16 -0400

    replace href attribute with routerLink

--- my-app/src/app/pages/create-new-user/create-user.component.ts
+++ my-app/src/app/pages/create-new-user/create-user.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CreateUserFormComponent } from '../../shared/create-user-page/create-user-form/create-user-form.component';
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'create-user',
   standalone: true,
-  imports: [CreateUserFormComponent],
+  imports: [CreateUserFormComponent, RouterLink],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
 })
--- my-app/src/app/pages/login/login.component.ts
+++ my-app/src/app/pages/login/login.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { LoginFormComponent } from '../../shared/login-page/login-form/login-form.component';
+import { Component } from '@angular/core'
+import { LoginFormComponent } from '../../shared/login-page/login-form/login-form.component'
+import { RouterLink } from '@angular/router'
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 
 export class LoginComponent {
