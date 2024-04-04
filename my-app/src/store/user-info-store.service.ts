e9c6b647ad892423fa6fc66c885ef00ad667896d
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-03 23:46:40 -0400

    Added reset user value method

--- my-app/src/store/user-info-store.service.ts
+++ my-app/src/store/user-info-store.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@angular/core'
+import { Injectable, inject } from '@angular/core'
 import { User } from '../models/user.model'
+import { UserDalService } from '../services/database/user.dal.service'
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserInfoService {
+  private uDalService = inject(UserDalService)
   private user: User = new User('', '')
+
+  resetUserValue(username : string){
+     this.uDalService.findUser(username).then(data =>{
+      this.user = data as User
+    })
+  }
 
   setUser(user: User) {
     this.user = user as User
