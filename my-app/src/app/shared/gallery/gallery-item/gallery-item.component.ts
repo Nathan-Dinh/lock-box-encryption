25c9bccbad1d1cafd9d6e16481bfe4ffe80852be
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-05 17:37:04 -0400

    Added a click event to return current gallery item location

--- my-app/src/app/shared/gallery/gallery-item/gallery-item.component.ts
+++ my-app/src/app/shared/gallery/gallery-item/gallery-item.component.ts
@@ -1,14 +1,22 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core'
+import { Component, ElementRef, HostListener, inject } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { GalleryItemLocationService } from '../../../../services/gallery-item-location.service'
+import { elementAt } from 'rxjs'
 
 @Component({
   selector: 'gallery-item',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './gallery-item.component.html',
   styleUrl: './gallery-item.component.css',
 })
 export class GalleryItemComponent {
-
-
-
+  gilService = inject(GalleryItemLocationService)
+  constructor(private el: ElementRef) {}
+  
+  onClick() {
+    const ElEMENT = this.el.nativeElement.getBoundingClientRect()
+    this.gilService.setLocation(ElEMENT)
+  }
 }
