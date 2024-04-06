3a1a7b95501d84cb2c5b530de6d0b52fd6f93a0a
John Doe <nathan.dinh03@outlook.com>
2024-04-05 20:31:04 -0400

    Improved code readability

--- my-app/src/app/shared/gallery/gallery-item/gallery-item.component.ts
+++ my-app/src/app/shared/gallery/gallery-item/gallery-item.component.ts
@@ -2,7 +2,6 @@ import { Component, ElementRef, HostListener, inject } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
 import { GalleryItemLocationService } from '../../../../services/gallery-item-location.service'
-import { elementAt } from 'rxjs'
 
 @Component({
   selector: 'gallery-item',
@@ -12,11 +11,11 @@ import { elementAt } from 'rxjs'
   styleUrl: './gallery-item.component.css',
 })
 export class GalleryItemComponent {
-  gilService = inject(GalleryItemLocationService)
+  private gilService = inject(GalleryItemLocationService)
   constructor(private el: ElementRef) {}
-  
+
   onClick() {
-    const ElEMENT = this.el.nativeElement.getBoundingClientRect()
-    this.gilService.setLocation(ElEMENT)
+    const COORDINATES : object = this.el.nativeElement.getBoundingClientRect() as object
+    this.gilService.setCoordinates(COORDINATES)
   }
 }
