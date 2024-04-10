a601720b098e84242727b29e844ae1788e781ec2
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-10 01:58:15 -0400

    Don't know

---
+++ my-app/src/app/shared/gallery-page/gallery-item/gallery-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, HostListener, inject } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { GalleryItemLocationService } from '../../../../services/observable/gallery-item-location.service'
+
+@Component({
+  selector: 'gallery-item',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './gallery-item.component.html',
+  styleUrl: './gallery-item.component.css',
+})
+export class GalleryItemComponent {
+  private gilService = inject(GalleryItemLocationService)
+  constructor(private el: ElementRef) {}
+
+  onClick() {
+    const COORDINATES: object =
+      this.el.nativeElement.getBoundingClientRect() as object
+    this.gilService.setCoordinates(COORDINATES)
+  }
+}
