77aa30283c49b62dd31c919ce9075f5264bc98b7
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-10 20:51:51 -0400

    Moved form logic to camera form component

--- my-app/src/app/pages/home/camera/camera.component.ts
+++ my-app/src/app/pages/home/camera/camera.component.ts
@@ -1,50 +1,28 @@
 import { Component, inject } from '@angular/core'
+import { TopHeaderNavComponent } from '../../../shared/components/top-header-nav/top-header-nav.component'
 import { CameraFormComponent } from '../../../shared/camera-page/camera-form/camera-form.component'
 import { CameraService } from '../../../../services/camera/camera.service'
-import { GalleryDalService } from '../../../../services/database/gallery.dal.service'
 import { FormsModule } from '@angular/forms'
-import { Gallery } from '../../../../models/gallery.model'
-import { Router } from '@angular/router'
 
 @Component({
   selector: 'camera-sub-page',
   standalone: true,
   imports: [
     FormsModule,
+    CameraFormComponent,
+    TopHeaderNavComponent
   ],
   templateUrl: './camera.component.html',
   styleUrl: './camera.component.css',
 })
 
 export class CameraComponent {
-  imgsrc: string | null
-  title: string = ''
-  date: Date = new Date()
-  description: string = ''
-  cameraService = inject(CameraService)
-  router = inject(Router)
+  private cameraService = inject(CameraService)
+  public imgsrc: any
 
-  constructor(private galleryDalService: GalleryDalService) {
-    this.cameraService = inject(CameraService)
+  constructor() {
+    alert("Trigger")
     this.imgsrc = this.cameraService.getCapturedImage()
   }
-
-  savePhoto(): void {
-    const galleryItem = new Gallery(this.date, this.title, this.description, this.imgsrc)
-
-    this.galleryDalService.insertGallery(galleryItem)
-      .then(
-        () => {
-          console.log('Photo saved')
-          localStorage.removeItem('photo')
-          this.router.navigate(['home/gallery'])
-        },
-      )
-      .catch((err) => {
-          console.error('Error saving photo', err)
-          localStorage.removeItem('photo')
-          this.router.navigate(['home/gallery'])
-        },
-      )
-  }
+  
 }
