3fb6b9602d7d6313791c16a26b11ed2c1eb4090b
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-11 04:34:27 -0400

    Added picture content top header nav

---
+++ my-app/src/app/shared/picture-content-page/top-header-nav/top-header-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pic-top-header-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-header-nav.component.html',
+  styleUrl: './top-header-nav.component.css'
+})
+export class TopHeaderNavComponent {
+
+}
