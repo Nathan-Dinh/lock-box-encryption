56fb744ca26705c3304513790193682bccd7fa12
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-12 00:41:21 -0400

    Deleted comments and added edit and delete gallery item logic

--- my-app/src/services/database/user-gallery.dal.service.ts
+++ my-app/src/services/database/user-gallery.dal.service.ts
@@ -28,7 +28,7 @@ export class UserGalleryDalService {
     })
   }
 
-  findGalleryItem(id: string, userName: string){
+  findGalleryItem(id: string, userName: string) {
     return new Promise((resolve, reject) => {
       const TRAN = this.lbedb.db.transaction(['users'], 'readwrite')
       const USER_STORE = TRAN.objectStore('users')
@@ -45,87 +45,44 @@ export class UserGalleryDalService {
     })
   }
 
-  // async retrieveGalleries(): Promise<PictureItem[]> {
-  //   return new Promise((resolve, reject) => {
-  //     try {
-  //       const transaction = this.lbedb.db.transaction(
-  //         ['users'],
-  //         'readonly'
-  //       )
-  //       const galleryStore = transaction.objectStore('users')
-  //       const request = galleryStore.openCursor()
-  //       const galleries: PictureItem[] = []
+  deleteGalleryItem(id: string, userName: string) {
+    return new Promise((resolve, reject) => {
+      const TRAN = this.lbedb.db.transaction(['users'], 'readwrite')
+      const USER_STORE = TRAN.objectStore('users')
+      const REQ = USER_STORE.get(userName)
+      REQ.onsuccess = (event: any) => {
+        const USER = event.target.result as User
+        if (USER) {
+          delete USER.userGallery[id]
+          USER_STORE.put(USER)
+          resolve(event)
+        }
+      }
+      REQ.onerror = (event: any) => {
+        reject(event)
+      }
+    })
+  }
 
-  //       request.onsuccess = (event: any) => {
-  //         const cursor = event.target.result
-  //         if (cursor) {
-  //           galleries.push(cursor.value)
-  //           cursor.continue()
-  //         } else {
-  //           resolve(galleries)
-  //         }
-  //       }
-  //       request.onerror = (event: any) => {
-  //         reject('Error in retrieving galleries')
-  //       }
-  //     } catch (e) {
-  //       reject(e)
-  //     }
-  //   })
-  // }
+  editGalleryItem(pictureItem: PictureItem, userName: string) {
+    return new Promise((resolve, reject) => {
+      const TRAN = this.lbedb.db.transaction(['users'], 'readwrite')
+      const USER_STORE = TRAN.objectStore('users')
+      const REQ = USER_STORE.get(userName)
+      REQ.onsuccess = (event: any) => {
+        const USER = event.target.result as User
+        if (USER) {
+          console.log(pictureItem)
+          USER.userGallery[pictureItem.getId()] = pictureItem
+          USER_STORE.put(USER)
+          resolve(event)
+        }
+      }
+      REQ.onerror = (event: any) => {
+        reject(event)
+      }
+    })
+  }
 
-  // deleteGallery(galleryId: number): Promise<any> {
-  //   return new Promise((resolve, reject) => {
-  //     const transaction = this.lbedb.db.transaction(
-  //       ['user_gallery'],
-  //       'readwrite'
-  //     )
-  //     const galleryStore = transaction.objectStore('user_gallery')
-  //     const request = galleryStore.delete(galleryId)
 
-  //     request.onsuccess = (event: any) => {
-  //       resolve(event)
-  //     }
-  //     request.onerror = (event: any) => {
-  //       reject(event)
-  //     }
-  //   })
-  // }
-
-  // updateGallery(galleryItem: PictureItem): Promise<any> {
-  //   return new Promise((resolve, reject) => {
-  //     const transaction = this.lbedb.db.transaction(
-  //       ['user_gallery'],
-  //       'readwrite'
-  //     )
-  //     const galleryStore = transaction.objectStore('user_gallery')
-  //     const request = galleryStore.put(galleryItem)
-
-  //     request.onsuccess = (event: any) => {
-  //       resolve(event)
-  //     }
-  //     request.onerror = (event: any) => {
-  //       reject(event)
-  //     }
-  //   })
-  // }
-
-  // findGallery(galleryId: number): Promise<any> {
-  //   return new Promise((resolve, reject) => {
-  //     const transaction = this.lbedb.db.transaction(
-  //       ['user_gallery'],
-  //       'readonly'
-  //     )
-  //     const galleryStore = transaction.objectStore('user_gallery')
-  //     const request = galleryStore.get(galleryId)
-
-  //     request.onsuccess = (event: any) => {
-  //       const result = event.target.result
-  //       result ? resolve(result) : resolve(null)
-  //     }
-  //     request.onerror = (event: any) => {
-  //       reject(event)
-  //     }
-  //   })
-  // }
 }
