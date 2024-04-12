92c1c8a97028f3cda2b78c78e4dbed4f437303dd
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-12 00:42:04 -0400

    Added delete and edit click events

--- my-app/src/app/shared/picture-content-page/picture-content-form/picture-content-form.component.ts
+++ my-app/src/app/shared/picture-content-page/picture-content-form/picture-content-form.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute } from '@angular/router'
 import { UserGalleryDalService } from '../../../../services/database/user-gallery.dal.service'
 import { UserInfoService } from '../../../../store/user-info-store.service'
 import { PictureItem } from '../../../../models/picture-item.model'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'picture-content-form',
@@ -23,13 +24,16 @@ export class PictureContentFormComponent implements OnInit {
   private route = inject(ActivatedRoute)
   private ugDalService = inject(UserGalleryDalService)
   private uiService = inject(UserInfoService)
+  private router = inject(Router)
   public imgSrc: string
   public frmPicContent: any
   public desControl: FormControl
   public dateControl: FormControl
+  public pictureItem: any
 
   constructor() {
     this.imgSrc = ''
+    this.pictureItem = {}
     this.frmPicContent = this.frmBuilder.group({
       description: ['', []],
       date: new FormControl({ value: '', disabled: true }),
@@ -40,19 +44,42 @@ export class PictureContentFormComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     const ID_VALUE = this.route.snapshot.queryParamMap.get('id') as string
-    const GALLERY_ITEM = await this.ugDalService.findGalleryItem(
+    const GALLERY_ITEM = (await this.ugDalService.findGalleryItem(
       ID_VALUE,
       this.uiService.getUserName()
-    ) as any
+    )) as any
+    this.pictureItem = GALLERY_ITEM
     this.desControl.setValue(GALLERY_ITEM.description)
     this.dateControl.setValue(this.getDate(GALLERY_ITEM.date as Date))
     this.imgSrc = GALLERY_ITEM.imgData
   }
 
-  getDate(date:Date) {
+  private getDate(date: Date) {
     const year = date.getFullYear()
     const month = String(date.getMonth() + 1).padStart(2, '0') // January is 0!
     const day = String(date.getDate()).padStart(2, '0')
     return `${year}-${month}-${day}`
+  }
+
+  public deleteGalleryItem() {
+    if (confirm('Picture content will be lost')) {
+      this.ugDalService.deleteGalleryItem(
+        this.pictureItem.id,
+        this.uiService.getUserName()
+      )
+      this.router.navigate(['/home/gallery'])
+    }
+  }
+
+  public editGalleryItem() {
+    if (confirm('Is information correct')) {
+      const ITEM = new PictureItem(
+        this.pictureItem.imgData,
+        this.desControl.value,
+        this.pictureItem.date,
+        this.pictureItem.id
+      )
+      this.ugDalService.editGalleryItem(ITEM, this.uiService.getUserName())
+    }
   }
 }
