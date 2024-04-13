a47305278176ab5813e52ad69c30cf0a321f5606
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-12 20:05:22 -0400

    Added show map method to display map from lat and lng

--- my-app/src/services/geo/geo.service.ts
+++ my-app/src/services/geo/geo.service.ts
@@ -1,25 +1,51 @@
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs'
+
+declare var H: any
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GeoService {
+  constructor() {}
 
-  constructor() { }
   getCurrentLocation(): Promise<any> {
     return new Promise<any>((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition((position: any) => {
-        resolve({
-          lat: position.coords.latitude,
-          lon: position.coords.longitude
-        });
-      }, (e) => {
-        reject({code: e.code, message: e.message})
-      }, {
-        timeout: 1000,
-        maximumAge: 0,
-        enableHighAccuracy: true
-      });
-    });
+      navigator.geolocation.getCurrentPosition(
+        (position: any) => {
+          resolve({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          })
+        },
+        (e) => {
+          reject({ code: e.code, message: e.message })
+        },
+        {
+          timeout: 1000,
+          maximumAge: 0,
+          enableHighAccuracy: true,
+        }
+      )
+    })
+  }
+
+  showMap(lat: string, lng: string, container: HTMLElement) {
+    container.innerHTML = ''
+    const PlATFORM = new H.service.Platform({
+      apikey: 'urccOV2buSNx-OguAYvGG4jJ--DiEu2IJABPsNGRlFk',
+    })
+    const MAP_TYPE = PlATFORM.createDefaultLayers()
+    const MAP = new H.Map(container, MAP_TYPE.vector.normal.map, {
+      zoom: 15,
+      center: {
+        lng: lng,
+        lat: lat,
+      },
+    })
+
+    const icon = new H.map.Icon('../../assets/person-bounding-box.svg')
+    const MARKER = new H.map.Marker({ lat: lat, lng: lng }, { icon: icon })
+    MAP.addObject(MARKER)
   }
 }
