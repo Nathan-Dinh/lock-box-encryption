407b11ff2f25380690cd22b1baae0610ffd4fb8c
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-12 22:22:26 -0400

    Extracted and refactored code

--- my-app/src/app/shared/camera-page/camera-form/camera-form.component.ts
+++ my-app/src/app/shared/camera-page/camera-form/camera-form.component.ts
@@ -5,11 +5,8 @@ import { UserGalleryDalService } from '../../../../services/database/user-galler
 import { UserInfoService } from '../../../../store/user-info-store.service'
 import { Router } from '@angular/router'
 import { GeoService } from '../../../../services/geo/geo.service'
-
-interface geoLocation{
-  latitude: number 
-  longitude: number
-}
+import { DateService } from '../../../../services/date.service'
+import { GeoLocation } from '../../../../models/geo-location.model'
 
 @Component({
   selector: 'camera-form',
@@ -18,8 +15,6 @@ interface geoLocation{
   templateUrl: './camera-form.component.html',
   styleUrl: './camera-form.component.css',
 })
-
-
 export class CameraFormComponent {
   @Input() imgsrc: string
 
@@ -28,9 +23,9 @@ export class CameraFormComponent {
   private uiService = inject(UserInfoService)
   private ugDalService = inject(UserGalleryDalService)
   private geoService = inject(GeoService)
-  public geoLocation : geoLocation
+  private dService = inject(DateService)
+  public geoLocation: GeoLocation
   public itemForm: any
-  
 
   public desControl: FormControl
   public dateControl: FormControl
@@ -38,56 +33,54 @@ export class CameraFormComponent {
 
   constructor() {
     this.imgsrc = ''
-    this.geoLocation = {latitude: 0, longitude:0}
+    this.geoLocation = { latitude: 0, longitude: 0 }
+    const CUR_DATE = this.dService.getDateFormate(new Date())
     this.itemForm = this.frmBuilder.group({
       description: ['', []],
-      date: new FormControl({ value: this.setCurrentDate(), disabled: true }),
+      date: new FormControl({ value: CUR_DATE, disabled: true }),
       geolocation: new FormControl({ value: '', disabled: true }),
     })
     this.desControl = this.itemForm.controls['description'] as FormControl
     this.dateControl = this.itemForm.controls['date'] as FormControl
-    this.geolocationControl = this.itemForm.controls['geolocation'] as FormControl
+    this.geolocationControl = this.itemForm.controls[
+      'geolocation'
+    ] as FormControl
   }
 
   ngOnInit() {
     this.getLocation()
   }
 
-  public setCurrentDate() {
-    const today = new Date()
-    const year = today.getFullYear()
-    const month = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
-    const day = String(today.getDate()).padStart(2, '0')
-    return `${year}-${month}-${day}`
-  }
-
   private getLocation(): void {
-    this.geoService.getCurrentLocation().then(data => {
-      this.geoLocation = { latitude: data.lat, longitude: data.lon }
-      this.itemForm.controls['geolocation'].setValue(`latitude: ${this.geoLocation.latitude}, longitude: ${this.geoLocation.longitude}`)
-      this.itemForm.controls['geolocation'].disable()
-    }).catch(error => {
-      console.error('Error fetching location:', error)
-      this.itemForm.controls['geolocation'].setValue('Failed: ' + error)
-      this.itemForm.controls['geolocation'].disable()
-    })
+    this.geoService
+      .getCurrentLocation()
+      .then((data) => {
+        this.geoLocation = { latitude: data.lat, longitude: data.lon }
+        this.itemForm.controls['geolocation'].setValue(
+          `latitude: ${this.geoLocation.latitude}, longitude: ${this.geoLocation.longitude}`
+        )
+        this.itemForm.controls['geolocation'].disable()
+      })
+      .catch((error) => {
+        console.error('Error fetching location:', error)
+        this.itemForm.controls['geolocation'].setValue('Failed: ' + error)
+        this.itemForm.controls['geolocation'].disable()
+      })
   }
 
   public savePhoto(): void {
     const ID: string = 'id' + Math.random().toString(16).slice(2)
     const DES_VALUE = this.desControl.value
-    const DATE_PARTS = this.dateControl.value.split('-')
-    const Y = parseInt(DATE_PARTS[0])
-    const M = parseInt(DATE_PARTS[1]) - 1 // Month is zero-based (0-11)
-    const D = parseInt(DATE_PARTS[2])
-
-    const DATE_VALUE: Date = new Date(Y, M, D) as Date
+    const DATE_VALUE: Date = this.dService.getDateFromFormat(
+      this.dateControl.value
+    )
     const PICTURE_ITEM: PictureItem = new PictureItem(
       this.imgsrc,
       DES_VALUE,
-      this.geoLocation,
+      new GeoLocation(this.geoLocation.longitude, this.geoLocation.latitude),
       DATE_VALUE,
-      ID)
+      ID
+    )
     this.ugDalService
       .insertGallery(this.uiService.getUserName(), PICTURE_ITEM)
       .then(() => {
