3780f853e2b98901affb00de190d0156761964d0
Nathan Dinh <nathan.dinh03@outlook.com>
2024-04-14 14:29:34 -0400

    Added new method to find encrypted item

--- my-app/src/services/database/user-encrypted-file.dal.service.ts
+++ my-app/src/services/database/user-encrypted-file.dal.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core'
 import { DatabaseService } from './database.service'
-import { UserEncryptedFile } from '../../models/user-encrypted-files.model'
+import { UserEncryptedItems } from '../../models/user-encrypted-items.model'
 import { EncryptedItem } from '../../models/encrypted-item.model'
 
 @Injectable({
@@ -9,7 +9,29 @@ import { EncryptedItem } from '../../models/encrypted-item.model'
 export class UserEncryptedFileDalService {
   private lbedb = inject(DatabaseService)
 
-  createUserEncryptFile(userEncryptedFile: UserEncryptedFile) {
+  findEncryptedItem(userName: string, id: string) {
+    return new Promise((resolve, reject) => {
+      const TRAN = this.lbedb.db.transaction(
+        ['user_encrypted_files'],
+        'readwrite'
+      )
+      const USER_GALLERY_STORE = TRAN.objectStore('user_encrypted_files')
+      const REQ = USER_GALLERY_STORE.get(userName)
+      REQ.onsuccess = (event: any) => {
+        const USER = event.target.result as UserEncryptedItems
+        if (USER) {
+          resolve(USER.encryptedFiles[id])
+        } else {
+          reject(event)
+        }
+      }
+      REQ.onerror = (event: any) => {
+        reject(event)
+      }
+    })
+  }
+
+  createUserEncryptFile(userEncryptedFile: UserEncryptedItems) {
     return new Promise((resolve, reject) => {
       const TRAN = this.lbedb.db.transaction(
         ['user_encrypted_files'],
@@ -31,7 +53,7 @@ export class UserEncryptedFileDalService {
       const USER_GALLERY_STORE = TRAN.objectStore('user_encrypted_files')
       const REQ = USER_GALLERY_STORE.get(userName)
       REQ.onsuccess = (event: any) => {
-        const USER = event.target.result as UserEncryptedFile
+        const USER = event.target.result as UserEncryptedItems
         if (USER) {
           USER.encryptedFiles[encryptedItem.id] = encryptedItem
           USER_GALLERY_STORE.put(USER)
@@ -55,7 +77,7 @@ export class UserEncryptedFileDalService {
       const USER_GALLERY_STORE = TRAN.objectStore('user_encrypted_files')
       const REQ = USER_GALLERY_STORE.get(userName)
       REQ.onsuccess = (event: any) => {
-        const USER = event.target.result as UserEncryptedFile
+        const USER = event.target.result as UserEncryptedItems
         if (USER) {
           delete USER.encryptedFiles[id]
           USER_GALLERY_STORE.put(USER)
