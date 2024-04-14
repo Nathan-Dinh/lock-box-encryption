6bb995155a70553d78aa55e6910cd163b7abf65e
JindoKimKor <jindokim.kor@gmail.com>
2024-04-14 13:35:17 -0400

    checking TypeScript for user-info-store.service.ts

--- my-app/src/store/user-info-store.service.ts
+++ my-app/src/store/user-info-store.service.ts
@@ -2,29 +2,28 @@ import { Injectable, inject } from '@angular/core'
 import { User } from '../models/user.model'
 import { UserDalService } from '../services/database/user.dal.service'
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class UserInfoService {
-  private uDalService = inject(UserDalService)
+  private uDalService: UserDalService = inject(UserDalService)
   private user: User = new User('', '')
 
-  public resetUserValue(username: string) {
+  public resetUserValue(username: string): void {
     this.uDalService.find(username).then((data) => {
       this.user = data as User
     })
   }
 
-  public setUser(user: User) {
+  public setUser(user: User): void {
     this.user = user as User
   }
 
-  public getUserName() {
+  public getUserName(): string {
     return this.user.userName
   }
 
-  public getUser() {
+  public getUser(): User {
     return this.user
   }
 }
