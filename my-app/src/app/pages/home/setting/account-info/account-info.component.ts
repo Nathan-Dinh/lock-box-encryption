4f3a18426057ababbc832fed1aae92a4cc34e9b3
Nathan Dinh <nathan.dinh03@outlook.com>
2024-05-01 13:28:07 -0400

    Implemented account info page layout

--- my-app/src/app/pages/home/setting/account-info/account-info.component.ts
+++ my-app/src/app/pages/home/setting/account-info/account-info.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIcon } from '@angular/material/icon'
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-account-info',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIcon, MatSelectModule, MatInputModule, MatFormFieldModule],
   templateUrl: './account-info.component.html',
-  styleUrl: './account-info.component.css'
+  styleUrl: './account-info.component.css',
 })
 export class AccountInfoComponent {
+  public passwordChangeStatus: string
 
+  constructor() {
+    this.passwordChangeStatus = "none"
+  }
+
+  showPassChangeClick(){
+    this.passwordChangeStatus = "block"
+  }
+
+  closePassChangeClick(){
+    this.passwordChangeStatus = "none"
+  }
 }
