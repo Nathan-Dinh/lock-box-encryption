aa9e260946ebf1fb736df6d979a6a65f9f9b6f9f
Nathan Dinh <nathan.dinh03@outlook.com>
2024-05-02 16:06:49 -0400

    Updated content

--- my-app/src/app/pages/home/setting/database/database.component.ts
+++ my-app/src/app/pages/home/setting/database/database.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIcon } from '@angular/material/icon'
 
 @Component({
   selector: 'app-database',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIcon],
   templateUrl: './database.component.html',
-  styleUrl: './database.component.css'
+  styleUrl: './database.component.css',
 })
-export class DatabaseComponent {
-
-}
+export class DatabaseComponent {}
